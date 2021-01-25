d07ada9a1693d4e383caefeb160d03b771f6dfc7
Danilo Romano <risaddex@gmail.com>
2021-01-25 13:01:54 -0300

    Added pre2

---
+++ pre/pre2/contadorComponent.js
@@ -0,0 +1,55 @@
+const BTN_RENICIALIZAR = "btnReiniciar"
+const ID_CONTADOR = "contador"
+const VALOR_CONTADOR = 100
+const PERIODO_INTERVALO = 10
+
+
+class ContadorComponent {
+
+    constructor() {
+        this.inicializar()
+    }
+
+    prepararContadorProxy() {
+        const handler = {
+            set: (currentContext, propertyKey, newValue) => {
+                console.log({ currentContext, propertyKey, newValue })
+
+                currentContext[propertyKey] = newValue
+                return true
+            }
+        }
+        
+        const contador = new Proxy({
+            valor: VALOR_CONTADOR,
+            efetuarParada: () => {}
+        }, handler)
+
+        return contador
+        
+    }
+    atualizarTexto({elementoContador, contador}) {
+        const identificadorTexto = '$$contador'
+        const textoPadrao = `Começando em <strong>${identificadorTexto}</strong> segundos...`
+        elementoContador.innerHTML = textoPadrao.replace(identificadorTexto, contador.valor--)
+    }
+
+    inicializar() { 
+        console.log('inicializou!')
+        const elementoContador = document.getElementById(ID_CONTADOR)
+
+        const contador = this.prepararContadorProxy()
+        // contador.valor = 100
+        // contador.valor = 90
+        // contador.valor = 80
+        const argumentos = {
+            elementoContador,
+            contador
+        }
+
+        this.atualizarTexto(argumentos)
+        this.atualizarTexto(argumentos)
+        this.atualizarTexto(argumentos)
+        
+    }
+}
